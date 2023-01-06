4e52f87596334eb2144c0f336ed4ef637238e7cf
Jade Lester Ballester <jadelesterballester@gmail.com>
2023-01-06 09:27:58 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.sampleFunction = void 0;
+const sampleFunction = () => {
+    console.log("Hello Jade");
+};
+exports.sampleFunction = sampleFunction;
+(0, exports.sampleFunction)();
+const lambda = () => {
+    console.log("this is lambda exp");
+};
+lambda();
---
+++ index.ts
@@ -0,0 +1,12 @@
+export const sampleFunction = () =>
+{
+    console.log("Hello Jade")
+}
+
+sampleFunction();
+
+const lambda = () => {
+    console.log("this is lambda exp")
+}
+
+lambda() 
